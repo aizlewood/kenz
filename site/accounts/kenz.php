034782a7770539b2df751a7e8738526d534a8191
Aizlewood <jon@clearleft.com>
2016-03-11 13:18:16 +0000

    Kirby, gitignore

---
+++ site/accounts/kenz.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: kenz
+email: jaizlewood@carbongraffiti.com
+password: >
+  $2a$10$UGcBW0gIUlOYupW7k.PgS.JxPJqqjl90ZpkqO5ZKkQAa9dRGFwDRW
+language: en
+role: admin
