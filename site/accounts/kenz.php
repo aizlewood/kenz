858d28634c7748255bf517012c160fa255de6081
Aizlewood <jon@clearleft.com>
2016-03-11 13:34:37 +0000

    messing with themes

--- site/accounts/kenz.php
+++ site/accounts/kenz.php
@@ -3,6 +3,6 @@
 username: kenz
 email: jaizlewood@carbongraffiti.com
 password: >
-  $2a$10$UGcBW0gIUlOYupW7k.PgS.JxPJqqjl90ZpkqO5ZKkQAa9dRGFwDRW
+  $2a$10$RA1MVPg7WFmDdvO7OHgUdeEjsi7fmawUtk/EcVeq4/BlsVYba/2Vq
 language: en
 role: admin
