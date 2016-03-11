65213659777a30c5b40f68d7b010affc3db04113
Aizlewood <jon@clearleft.com>
2016-03-11 13:36:17 +0000

    New theme

--- site/accounts/kenz.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: kenz
-email: jaizlewood@carbongraffiti.com
-password: >
-  $2a$10$RA1MVPg7WFmDdvO7OHgUdeEjsi7fmawUtk/EcVeq4/BlsVYba/2Vq
-language: en
-role: admin
